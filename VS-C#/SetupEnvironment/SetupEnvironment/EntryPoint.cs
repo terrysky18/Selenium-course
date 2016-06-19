b626cf2307c605f786b1d67c4c884802c37f6416
Terry Song <Terry Song>
2016-06-19 00:18:32 -0400

    added Visual Studio C# projects

---
+++ VS-C#/SetupEnvironment/SetupEnvironment/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System.Threading;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        TestChrome();       //open page in Chrome
+        Thread.Sleep(1000);
+        TestIE();           //open page in Internet Explorer
+    }
+
+    static void TestChrome()
+    {
+        IWebDriver driver = new ChromeDriver();     //initialise webdriver
+        driver.Navigate().GoToUrl("http://testing.todorvachev.com");
+        Thread.Sleep(3000);     //pause for 3 seconds
+        driver.Quit();
+    }
+
+    static void TestIE()
+    {
+        IWebDriver driver = new InternetExplorerDriver();     //initialise webdriver
+        driver.Navigate().GoToUrl("http://testing.todorvachev.com");
+        Thread.Sleep(3000);     //pause for 3 seconds
+        driver.Quit();
+    }
+}
