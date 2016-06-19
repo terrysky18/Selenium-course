101871361240a52b3d378f644e6986f6a492864c
Terry Song <Terry Song>
2016-06-19 15:06:43 -0400

    added selector exercise

---
+++ VS-C#/Selenium-Selector/Selectors.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System.Threading;
+
+class Selectors
+{
+    static void Main()
+    {
+        SelectorByID();
+    }
+
+    private static void GreenMessage(string message)
+    {
+        // set foreground colour
+        System.Console.ForegroundColor = System.ConsoleColor.Green;
+        System.Console.WriteLine(message);
+        // reset foreground colour to default
+        System.Console.ForegroundColor = System.ConsoleColor.White;
+    }
+
+    private static void RedMessage(string message)
+    {
+        // set foreground colour
+        System.Console.ForegroundColor = System.ConsoleColor.Red;
+        System.Console.WriteLine(message);
+        // reset foreground colour to default
+        System.Console.ForegroundColor = System.ConsoleColor.White;
+    }
+
+    static void SelectorByName()
+    {
+        IWebDriver browser = new ChromeDriver();
+        browser.Navigate().GoToUrl("http://testing.todorvachev.com/selectors/name/");
+
+        //find the elements in the code
+        IWebElement element = browser.FindElement(By.Name("myName"));
+
+        if (element.Displayed)
+        {
+            // element found in the page
+            GreenMessage("Indeed, element found in the page");
+        }
+        else
+        {
+            // element not found
+            RedMessage("Element not found");
+        }
+        var name = System.Console.ReadLine();
+        browser.Quit();
+    }
+
+    static void SelectorByID()
+    {
+        string test_site = "http://testing.todorvachev.com/selectors/id/";
+        IWebDriver browser = new ChromeDriver();
+        browser.Navigate().GoToUrl(test_site);
+
+        string test_id = "testImage";
+        IWebElement element = browser.FindElement(By.Id(test_id));
+
+        if (element.Displayed)
+        {
+            // element found in the page
+            GreenMessage("Indeed, element found in the page");
+        }
+        else
+        {
+            RedMessage("Element not found");
+        }
+        var name = System.Console.ReadLine();
+        browser.Quit();
+    }
+}
--- VS-C#/SetupEnvironment/SetupEnvironment/EntryPoint.cs
+++ VS-C#/SetupEnvironment/SetupEnvironment/EntryPoint.cs
@@ -7,21 +7,21 @@ class EntryPoint
 {
     static void Main()
     {
-        TestChrome();       //open page in Chrome
-        Thread.Sleep(1000);
-        TestIE();           //open page in Internet Explorer
+        
     }
-
-    static void TestChrome()
+    
+    static void QuickTestChrome()
     {
+        // Open Chrome and navigate to a test page then close Chrome
         IWebDriver driver = new ChromeDriver();     //initialise webdriver
         driver.Navigate().GoToUrl("http://testing.todorvachev.com");
         Thread.Sleep(3000);     //pause for 3 seconds
         driver.Quit();
     }
 
-    static void TestIE()
+    static void QuickTestIE()
     {
+        // Open Internet Explorer and navigate to a test page then close IE
         IWebDriver driver = new InternetExplorerDriver();     //initialise webdriver
         driver.Navigate().GoToUrl("http://testing.todorvachev.com");
         Thread.Sleep(3000);     //pause for 3 seconds
